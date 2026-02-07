fa2eb200ece1c4ace909464a30f85bcee3b28f32
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2026-02-07 15:54:27 -0300

    Import CardLogin and CardCadastro components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-//import { CardLogin } from './components/card-login/card-login';
-//import { CardCadastro } from './components/card-cadastro/card-cadastro';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { CardLogin } from './components/card-login/card-login';
+import { CardCadastro } from './components/card-cadastro/card-cadastro';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [CardLogin, CardCadastro],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Estoque');
 }
+
